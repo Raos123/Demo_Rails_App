eba2be1a2fbd15f8b9f5dc78b5e16883b39a28d4
jroman <jroman@covermymeds.com>
2012-07-09 12:03:33 -0400

    Layout -Added custom css to finish general look of app
    Scaffolding
    -Remove User and Micropost scaffolding to make way for newer ones

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  attr_accessible :email, :name
-  has_many :microposts
-  
-  validates :name, :presence => true
-  
-  validates :email, :format =>{ :with => /^\w+@\w+\.com$/}
-end
